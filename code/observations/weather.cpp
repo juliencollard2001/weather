8d35cc83dc452e110dc3332dee3c5fb99f775c22
Julien <julien.collard@polytechnique.edu>
2023-04-07 15:56:43 +0200

    ras

--- code/observations/weather.cpp
+++ code/observations/weather.cpp
@@ -2,27 +2,26 @@
 
 #include <map>
 
-
 weather::weather(time_t t, double pressure, double tend_pressure, int tend_code,
-                int wind_direction, double wind_speed, double temperature,
-                double dew_point, double humidity) : observation(t) {
-            this->pressure = pressure;
-            this->tend_pressure = tend_pressure;
-            this->tend_code = tend_code;
-            this->wind_direction = wind_direction;
-            this->wind_speed = wind_speed;
-            this->temperature = temperature;
-            this->dew_point = dew_point;
-            this->humidity = humidity;
-        }
-
-
-std::vector<std::vector<double>> weather::code = {{0,-1}, {1,-1}, {1,0}, {1,1}, {0,0}, {0,1}, {-1, 1}, {-1,0},{-1,-1}};
-
-double weather::get_tend_first() {
-    return weather::code[tend_code][0];
+                 int wind_direction, double wind_speed, double temperature,
+                 double dew_point, double humidity)
+    : observation(t) {
+  this->pressure = pressure;
+  this->tend_pressure = tend_pressure;
+  this->tend_code = tend_code;
+  this->wind_direction = wind_direction;
+  this->wind_speed = wind_speed;
+  this->temperature = temperature;
+  this->dew_point = dew_point;
+  this->humidity = humidity;
 }
 
-double weather::get_tend_second() {
-    return weather::code[tend_code][1];
-}
+std::vector<std::vector<double>> weather::code = {{0, -1}, {1, -1}, {1, 0},
+                                                  {1, 1},  {0, 0},  {0, 1},
+                                                  {-1, 1}, {-1, 0}, {-1, -1}};
+
+double weather::get_tend_first() { return weather::code[tend_code][0]; }
+
+double weather::get_tend_second() { return weather::code[tend_code][1]; }
+
+double observation::dist(observation a, observation b) { return 0.0; }
--- code/sets/set.hpp
+++ code/sets/set.hpp
@@ -1,32 +1,16 @@
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 #include "../observations/observation.hpp"
 
 class set {
-    private:
-        std::vector<observation> * data;
-    public:
-        set() {
-            data = new std::vector<observation>();
-        }
-        set(std::vector<observation> _data) {
-            *data = _data;
-        }
-        void sort_by_time() {
-            std::sort(data->begin(), data->end(), [] (observation a , observation b) -> bool {return a.get_t() < b.get_t(); });
-        }
-        set filter_by_time(time_t a, time_t b) {
-            sort_by_time();
-            auto comp = [] (observation a ,  time_t t) -> bool {return a.get_t() < t; };
-            auto start = std::lower_bound(data->begin(), data->end(), a, comp);
-            auto end = std::upper_bound(data->begin(), data->end(), a, comp);
-            std::vector<observation> filt = std::vector<observation>(start, end);
-            return set(filt);
-        }
+private:
+  std::vector<observation> *data;
 
-        ~set() {
-            delete data;
-        }
-
+public:
+  set();
+  set(std::vector<observation> _data);
+  void sort_by_time();
+  set filter_by_time(time_t a, time_t b);
+  ~set();
 };
